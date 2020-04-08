fee66c673ef6b7ee5eb2da2a8e8bcff154e97cb9
unknown <levantuan6898@gmail.com>
2020-04-08 18:28:54 +0700

    add file

---
+++ src/LeapYear.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.println("Enter the year: ");
+        year = scanner.nextInt();
+
+        boolean isLeapYear = false;
+
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                if(year % 400 == 0){
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+
+        if(isLeapYear){
+            System.out.printf("%d is a leap year", year);
+        } else {
+            System.out.printf("%d is NOT a leap year", year);
+        }
+    }
+}
